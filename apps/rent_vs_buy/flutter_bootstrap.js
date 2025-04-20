1801b4f0ec1807849b5deafde387490d3f64e818
Jake Taylor <jakeetaylor@meta.com>
2025-04-20 19:04:42 +0000

    feat: add va funding fee to rentvbuy

--- apps/rent_vs_buy/flutter_bootstrap.js
+++ apps/rent_vs_buy/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "203622187"
+    serviceWorkerVersion: "2708820208"
   }
 });
